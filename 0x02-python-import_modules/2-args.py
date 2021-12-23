656ddb0b2ecf62e213c6f02322b1569f04ad1e96
SergioBalca <3896@holbertonschool.com>
2021-12-23 09:28:22 -0800

    0-add.py imports the function def add(a, b): from the file add_0.py and prints the result of the addition 1 + 2 = 3; 1-calculation.py imports functions from the file calculator_1.py and 2-args.py prints the number of and the list of its arguments

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import sys
+if __name__ == "__main__":
+    # no arguments passed to programm
+    if len(sys.argv) == 1:
+        print("{} arguments.".format(0))
+    # one argument passed to programm
+    elif len(sys.argv) == 2:
+        print("{} argument:".format(1))
+        print("{}: {}".format(1, sys.argv[1]))
+    elif len(sys.argv) > 2:
+        for i in range(1, len(sys.argv)):
+            print("{}: {}".format(i, sys.argv[i]))
