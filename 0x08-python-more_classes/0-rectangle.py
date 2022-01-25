d08e82de32dff9594c3c3ef0403282013e9e3c47
SergioBalca <3896@holbertonschool.com>
2022-01-24 18:26:54 -0800

    empty class Rectangle that defines a rectangle

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""Class Rectangle definition"""
+
+
+class Rectangle:
+
+    """Model of a Rectangle"""
+
+    def __init__(self):
+
+        """__init__ method for the Rectangle class"""
+
+        pass
