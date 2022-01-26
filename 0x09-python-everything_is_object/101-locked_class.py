e8f55b85e47b747fab4f2f7a669d1190c5e0b510
SergioBalca <3896@holbertonschool.com>
2022-01-25 20:50:10 -0800

    class that prevents the creation of instances

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
 
+"""Module to define a class LockedClass"""
+
+
 class LockedClass:
+
+    """prevents the creation of new instances
+    other than first_name"""
+
     __slots__ = ['first_name']
