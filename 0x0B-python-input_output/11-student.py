c241f0d92aed4aa885fe1983bdb5b1ffbc097ee5
SergioBalca <3896@holbertonschool.com>
2022-02-02 08:56:08 -0800

    method that replaces all attributes of a Student instance

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+
+""" this module contains the definition of class Student """
+
+
+class Student:
+
+    """ Student class """
+
+    def __init__(self, first_name, last_name, age):
+
+        """ constructor to initialize public instances:
+            first_name
+            last_name
+            age
+        """
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    """ public method that retrives a dictionary representation
+        of a student instance
+    """
+
+    def to_json(self, attrs=None):
+        attributes = {}
+        if type(attrs) is list:
+            for i in attrs:
+                if i in self.__dict__:
+                    attributes[i] = self.__dict__[i]
+            return attributes
+        return self.__dict__
+
+    """ publict method that replaces all attributes of the student
+        instance
+    """
+
+    def reload_from_json(self, json):
+        for key in json:
+            self.__dict__[key] = json[key]
