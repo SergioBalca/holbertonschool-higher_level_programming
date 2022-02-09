ac2dacacee7c1bee624aa1e94deff6716ea855c3
SergioBalca <3896@holbertonschool.com>
2022-02-09 10:14:14 -0500

    static method that returns the JSON string representation of list_dictionaries was added to Base class

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 
+import json
+
 """module that has the Base class"""
 
 
@@ -21,3 +23,13 @@ class Base:
             self.id = self.__class__.__nb_objects
         else:
             self.id = id
+
+    def to_json_string(list_dictionaries):
+
+        """ static method that returns the JSON string representation
+            of list_dictionaries
+        """
+        if list_dictionaries is None:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
