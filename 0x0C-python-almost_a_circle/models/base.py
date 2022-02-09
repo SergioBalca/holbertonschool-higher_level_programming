c9785a103f04e93a980f796ea2b015c6fb7867fc
SergioBalca <3896@holbertonschool.com>
2022-02-09 13:53:13 -0500

    static method that returns the JSON string representation of list_dictionaries was added to Base class

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -26,12 +26,21 @@ class Base:
 
     @staticmethod
     def to_json_string(list_dictionaries):
-
         """ static method that returns the JSON string representation
             of list_dictionaries
             Args:
                   list_dictinaries: a list of dictionaries
         """
+
         if list_dictionaries is None or len(list_dictionaries) == 0:
             return "[]"
         return json.dumps(list_dictionaries)
+
+    "_ANS = to_json_string.__func__()"
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+
+        """ class method that writes the JSON string
+            representation of list_obj to a file
+        """
