9d38f62b94725ef20690a9ddfe31ade7f94c1268
SergioBalca <3896@holbertonschool.com>
2022-03-15 08:41:08 -0700

    the command SHOW CREATE TABLE was used to print the full description of the table first_table

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,3 +1,3 @@
 -- script that prints the full description of the table first_table from database hbtn_0c_0 in MySQL server
 -- the database name is passed as an argument of the mysql command
-DESC first_table;
+SHOW CREATE TABLE first_table;
