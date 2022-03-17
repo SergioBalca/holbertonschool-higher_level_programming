5f3ed4341b3e121c8a503bb9448246c60960d9ad
SergioBalca <3896@holbertonschool.com>
2022-03-17 10:33:26 -0700

    the WHERE clause was deleted, because the INNER JOIN excludes the records where tv_show_genres.genre_id in NULL

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -7,5 +7,4 @@
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_show_genres INNER JOIN tv_shows
 ON tv_show_genres.show_id = tv_shows.id
-WHERE tv_show_genres.genre_id IS NOT NULL
 ORDER BY tv_shows.title, tv_show_genres.genre_id  ASC;
