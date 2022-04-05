7a22ce41c44575c070b7107e5ba09a4859dd187e
SergioBalca <3896@holbertonschool.com>
2022-04-04 19:34:33 -0700

    script that prints 3 lines by using an array of string and a loop

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-const stringArray = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+const stringArray = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 let count = 0;
 
 while (count <= 2) {
