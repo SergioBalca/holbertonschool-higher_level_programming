e2f230d259ee666dcd4bf07f926ba4314893c922
SergioBalca <3896@holbertonschool.com>
2022-04-12 08:30:17 -0700

    the endsWith method was used to filter the character

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -2,7 +2,7 @@
 
 const args = process.argv.slice(2);
 const request = require('request');
-const character = 'https://swapi-api.hbtn.io/api/people/18/';
+const character = '18/';
 
 request(args[0], (err, response) => {
   if (err) {
@@ -12,7 +12,7 @@ request(args[0], (err, response) => {
     let count = 0;
     for (let index = 0; index < obj.results.length; index++) {
       for (let idx = 0; idx < obj.results[index].characters.length; idx++) {
-        if (obj.results[index].characters[idx] === character) {
+        if (obj.results[index].characters[idx].endsWith(character)) {
           count++;
         }
       }
