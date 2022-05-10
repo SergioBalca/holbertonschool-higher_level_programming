c88c84c22c5be01baee779d0a4235f7d4ef94614
SergioBalca <3896@holbertonschool.com>
2022-05-10 09:27:41 -0700

    script that takes in arguments and displays all values in the states table of hbtn_0e_0_usa where name mataches the argument that is safe from MySQL injections

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+
+""" Takes arguments and displays all values in the states table
+    of hbtn_0e_0_usa where name matches the argument, safe
+    from MySQL injections
+"""
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    user_name = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+    state = sys.argv[4]
+
+    """ To connect to the MYSQL server on port 3306 (default) """
+    con = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=user_name,
+            passwd=password,
+            db=db_name
+        )
+
+    """ To get a cursor """
+    cur = con.cursor()
+    cur.execute("""SELECT *
+                FROM states
+                WHERE name = %(name)s
+                ORDER BY id ASC""", {'name': state})
+
+    """ Fetchall to get the results """
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    con.close()
