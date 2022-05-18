55c21f0b3853479665206cf2d5020d5d176d640a
SergioBalca <3896@holbertonschool.com>
2022-05-18 13:27:50 -0500

    Readme file added

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Takes in a URL, sends a request to that URL, ana displays the size
-curl -sI "$1" | awk '/Content-Lentgh/ {print $2}'
+curl -sI "$1" | awk '/Content-Lentgh/ { print $2 }'
