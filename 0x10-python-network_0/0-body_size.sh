14b1a557bb584c3aa2656312d63c3501c7905999
SergioBalca <3896@holbertonschool.com>
2022-05-18 11:44:04 -0700

    typo fixed

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Takes in a URL, sends a request to that URL, ana displays the size
+# Takes in a URL, sends a request to that URL, ana displays its size
 curl -sI "$1" | awk '/Content-Length/ { print $2 }'
