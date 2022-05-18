352da591b994f486a2cc1cec7336129e09936bb5
SergioBalca <3896@holbertonschool.com>
2022-05-18 12:52:11 -0500

    Readme file added

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-# Takes in a URL, sends a request to that URL, and
-# displays the size of the body of the response
-# to get a single header, the grep command is used
-
+# Takes in a URL, sends a request to that URL, ana displays the size
 curl -s -I "$1" | grep Content-Length | cut -d ":" -f 2 | cut -d " " -f 2
