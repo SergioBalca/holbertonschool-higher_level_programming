4b346e665e1fccdda2c4dd276e74280a047fb3b7
SergioBalca <3896@holbertonschool.com>
2022-05-18 21:56:00 -0500

    pycodestyle error fixed

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Module with find_peak function"""
 
+
 def find_peak(list_of_integers):
     """ function that finds a peak in a
         list of unsorted integers
@@ -10,4 +11,3 @@ def find_peak(list_of_integers):
         return list_of_integers[len(list_of_integers) - 1]
     else:
         return None
-
