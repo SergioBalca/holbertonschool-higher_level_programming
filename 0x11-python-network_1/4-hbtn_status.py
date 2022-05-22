63be18adcb91719fe0cbf32992ae8e34ef026471
SergioBalca <git@github.com:SergioBalca>
2022-05-22 08:06:49 -0700

    script that fetches https://intranet.hbtn.io/status

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-"""fetches https://intranet.hbtn.io/status"""
+"""
+Script that fetches https://intranet.hbtn.io/status
+"""
 
 from requests import get
 
 if __name__ == "__main__":
-
-    url = 'https://intranet.hbtn.io/status'
-    response = get(url)
-    print('Body response:')
-    print('\t- type:', type(response.text))
-    print('\t- content:', response.content)
+    response = get('https://intranet.hbtn.io/status')
+    print("Body response:")
+    print("\t- type:", type(response.text))
+    print("\t- content:", response.text)
