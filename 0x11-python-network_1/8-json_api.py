6cc5493a2ec8f8958970a7511b2e89fb890fea75
SergioBalca <git@github.com:SergioBalca>
2022-05-23 08:53:09 -0700

    script that takes in a letter and sends a POST request to http://0.0.0.0:5000/search_user with the letter as a parameter

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+""" takes in a letter and sends a POST request to
+    http://0.0.0.0:5000/search_user with the letter
+    as a parameter.
+"""
+
+from requests import post
+from sys import argv
+
+
+if __name__ == "__main__":
+
+    url = 'http://0.0.0.0:5000/search_user'
+
+    if len(argv) == 1:
+        letter = ""
+    else:
+        letter = argv[1]
+
+    response = post(url, data={'q': letter})
+    header = response.headers['content-type']
+    json = response.json()
+
+    if header != 'application/json':
+        print('Not a valid JSON')
+    elif len(json) == 0:
+        print('No result')
+    else:
+        print('[{}] {}'.format(json['id'], json['name']))
